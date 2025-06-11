860ed99d529f5e6f3da332511be267100b0e2ff1
vi.nguyendt@cloudgo.vn <nguyenduongthevi31415@gmail.com>
2025-06-11 09:25:57 +0700

    Update footer

--- src/components/Common/Footer/Footer.js
+++ src/components/Common/Footer/Footer.js
@@ -1,138 +1,33 @@
 import React from "react";
-import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
-import Logo from "../../image/LogoVCineP.png";
+import { Facebook, Twitter, Youtube } from "lucide-react";
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-waves">
-        <div className="wave wave1"></div>
-        <div className="wave wave2"></div>
-        <div className="wave wave3"></div>
-      </div>
+    <footer className="bg-black text-white relative overflow-hidden">
+   
 
-      <div className="container">
-        <div className="footer-grid">
-          <div className="footer-column">
-            <h3 className="footer-heading">GIỚI THIỆU</h3>
-            <ul className="footer-links">
-              <li>
-                <a href="#" className="hover-effect">
-                  Về Chúng Tôi
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover-effect">
-                  Thoả Thuận Sử Dụng
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover-effect">
-                  Quy Chế Hoạt Động
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover-effect">
-                  Chính Sách Bảo Mật
-                </a>
-              </li>
-            </ul>
+      <div className="container mx-auto px-4 py-6">
+  
+        {/* Company Info */}
+        <div className="text-center border-gray-800 ">
+          <h4 className="text-xl font-bold text-red-500 mb-4 animate-glow">
+            CÔNG TY CỔ PHẦN PHIM VCINEP
+          </h4>
+          <div className="mb-2">
+            <p className="text-gray-400">Tầng 5, Số 157 Đường Nguyễn Đình Chiểu,Phường 6, Quận 3, TP. Hồ Chí Minh</p>
           </div>
-
-          <div className="footer-column">
-            <h3 className="footer-heading">GÓC ĐIỆN ẢNH</h3>
-            <ul className="footer-links">
-              <li>
-                <a href="#" className="hover-effect">
-                  Thể Loại Phim
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover-effect">
-                  Bình Luận Phim
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover-effect">
-                  Blog Điện Ảnh
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover-effect">
-                  Phim Hay Tháng
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover-effect">
-                  Phim IMAX
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="footer-column">
-            <h3 className="footer-heading">HỖ TRỢ</h3>
-            <ul className="footer-links">
-              <li>
-                <a href="#" className="hover-effect">
-                  Góp Ý
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover-effect">
-                  Sale & Services
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover-effect">
-                  Rạp / Giá Vé
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover-effect">
-                  Tuyển Dụng
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover-effect">
-                  FAQ
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="footer-column">
-            <div className="social-icons">
-              <a href="#" className="social-icon facebook">
-                <FaFacebook />
-                <span className="tooltip">Facebook</span>
-              </a>
-              <a href="#" className="social-icon twitter">
-                <FaTwitter />
-                <span className="tooltip">Twitter</span>
-              </a>
-              <a href="#" className="social-icon youtube">
-                <FaYoutube />
-                <span className="tooltip">YouTube</span>
-              </a>
-            </div>
-            <div className="logo-container">
-              <img src={Logo} alt="Logo" className="footer-logo pulse" />
-            </div>
-          </div>
-        </div>
-
-        <div className="company-info">
-          <h4 className="glow-text">CÔNG TY CỔ PHẦN VCINEP</h4>
-          <p>Nguyễn Đại Phúc - Nguyễn Dương Thế Vĩ</p>
-          <p className="contact-info">
-            <span className="highlight">028.39.333.303</span> •
-            <span className="highlight">19002224</span> (9:00 - 22:00) •
-            <span className="highlight">hotro@vcinep.vn</span>
+          <p className="mb-2">Nguyễn Đại Phúc - Nguyễn Dương Thế Vĩ</p>
+          <p className="mb-4">
+            <span className="text-red-500">028.39.333.303</span> •{" "}
+            <span className="text-red-500">19002224</span> (9:00 - 22:00) •{" "}
+            <span className="text-red-500">hotro@vcinep.vn</span>
           </p>
+          <p className="text-gray-500">Phiên bản 6.11.2025</p>
+           <p className="text-gray-500">© 2025 VCINEP. All rights reserved.</p>
         </div>
       </div>
+
     </footer>
   );
 };
